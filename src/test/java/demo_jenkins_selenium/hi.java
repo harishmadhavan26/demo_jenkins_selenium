60b2117a995c444b8bf3a87318277748bbb86cc3
Harish Madhavan <harishmadhavan26@gmail.com>
2026-03-16 10:51:02 +0530

    Added hi.java and updated testng.xml

---
+++ src/test/java/demo_jenkins_selenium/hi.java
@@ -0,0 +1,14 @@
+package demo_jenkins_selenium;
+
+
+import org.testng.annotations.Test;
+
+public class hi {
+	
+	@Test(groups = "smoke")
+	public void sample1() 
+	{
+		System.out.println("hi");
+	}
+
+}
